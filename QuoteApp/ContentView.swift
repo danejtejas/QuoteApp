2087888e6788ed3fe9347a87f3d5ac1dd6595c62
tejas <danej.tejash0@gmail.com>
2026-04-18 11:44:41 +0530

    Initial Commit

---
+++ QuoteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteApp
+//
+//  Created by Tejas on 18/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteApp/QuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppApp.swift
+//  QuoteApp
+//
+//  Created by Tejas on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct QuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
